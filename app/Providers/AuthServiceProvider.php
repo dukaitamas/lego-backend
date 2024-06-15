12a3931ae2220a82c04e55ca16d23ec539851e40
Tamas Dukai <tomi.2@hotmail.co.uk>
2024-06-15 23:51:04 +0200

    próbálkozások load more és paginationre

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Access\Gate;
+use Illuminate\Contracts\Auth\Access\Gate as AccessGate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate as FacadesGate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+         'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -23,8 +27,23 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        
+    //     //
+     }
 
-        //
+//     public function boot(): void
+// {
+//     $this->registerPolicies();
+
+//     Gate::define('update-post', function ($user, $post) {
+//         return $user->id === $post->user_id;
+//     });
+// }
+
+// Auth::viaRequest('custom-token', function ($request) {
+//     return User::where('api_token', $request->token)->first();
+// });
+
+// }
+
+
     }
-}
